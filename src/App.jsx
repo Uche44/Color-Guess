00582f7cbaeb0c8dff3c4da919e3c5a9aa134e96
Uche44 <perpetualuchechukwu808@gmail.com>
2025-10-11 09:50:46 +0100

    converting to miniapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
+import sdk from "@farcaster/miniapp-sdk";
 import ColorGuessGame from "./components/ColorGuessGame";
 
 const App = () => {
+  const loadApp = async () => {
+    await sdk.actions.ready();
+  };
+
+  useEffect(() => {
+    loadApp();
+  }, []);
+
   return (
     <div>
       <ColorGuessGame />
---
+++ src/constants/gameInstructions.js
@@ -0,0 +1,7 @@
+ export const gameInstructions = [
+    "Click on the start button to begin the game",
+    "Once the game starts, a color is randomly selected",
+    "You are to guess the selected color by clicking on the color palettes",
+    "After each coice you make, another color is selected. You are allowed a maximum of ten guesses",
+    "May the best man win. Goodluck!!",
+  ];
